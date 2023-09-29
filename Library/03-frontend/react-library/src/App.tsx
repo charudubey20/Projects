57fb7203e3290737b5b4fd7cb417fb8a4878ee59
charu.dubey <charu.dubey@broadridge.com>
2023-09-29 17:47:52 +0530

    initial commit

--- Library/03-frontend/react-library/src/App.tsx
+++ Library/03-frontend/react-library/src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import { Navbar } from './layouts/NavbarAndFooter/Navbar';
+import { Footer } from './layouts/NavbarAndFooter/Footer';
+import { HomePage } from './layouts/HomePage/HomePage';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Welcome to the full stack course</h1>
-    </div>
-  );
-}
+    <div>
+   <Navbar/>
+   <HomePage/>
+   <Footer/>
+   </div>
+  )};
 
 export default App;
